df8bcfe85da4cdee31cfc51e4e9a22276de5cb33
Yash Agarwal <yashagr@Yashs-MacBook-Air.local>
2023-08-07 00:16:38 +0530

    modified meta

---
+++ constants/data.js
@@ -0,0 +1,71 @@
+const userData = {
+  githubUsername: "manuarora700",
+  name: "Yash Agarwal",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "yash.ag.bu@gmail.com",
+  phone: "+91 9548481504",
+  address: "NOIDA, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 19 years old. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Node.js, Express, MongoDB, MySQL, PosGres, Bootstrap, Tailwind, C, C++, Kotin and Java programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Developer Intern",
+      company: "Rapipay Fintech Pvt. Ltd.",
+      year: "2023",
+      companyLink: "https://in.rapipay.com/",
+      desc: "Contributed to APIs in wallet team for our flagship product - NYE - which is the coolest banking experience that will give you more control over your money, savings and spends.",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "Scaler Academy",
+      year: "2022",
+      companyLink: "https://www.scaler.com/",
+      desc: "Developed ReactJS UI for web components (React, HTML and CSS) and templates provided in form of figma files.",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "GlueLabs",
+      year: "2022",
+      companyLink: "https://gluelabs.com/",
+      desc: "Got a training in frontend library React from professionals and worked with them by creating responsive React UI.",
+    },
+    {
+      title: "Graduation",
+      company: "Bennett University, Greater NOIDA, Uttar Pradesh",
+      year: "2019",
+      companyLink: "https://www.bennett.edu.in/",
+      desc: "Major in Computer Science and Engineering with a CGPA of 9.1. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Maria Assumpta Sr. Sec. School, Kashipur.",
+      year: "2018",
+      companyLink: "https://mariaschoolkashipur.org/",
+      desc: "Subjects being PCM with CS and English - fortunately school topper with 96.4% marks aggregate. .",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/16TN0b4xGLYkRG7Ijlp__CNti4bVcTL0Z/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/yash-agarwal-fast-nd-steady/",
+  },
+};
+
+export default userData;
